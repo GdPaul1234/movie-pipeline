88bbf1b6cef042310711f7fd4c59c252302e6c0e
GdPaul1234 <paul.godin1234@outlook.fr>
2022-12-12 19:56:50 +0100

    Clean ffmpeg_command_with_progress

--- lib/ffmpeg_with_progress.py
+++ lib/ffmpeg_with_progress.py
@@ -21,17 +21,14 @@ class ProgressItem(TypedDict):
 
 def ffmpeg_command_with_progress(command, cmd=['ffmpeg'], **args):
     with subprocess.Popen(command.compile(cmd=cmd), **args, text= True, stderr=subprocess.PIPE) as process:
-        while True:
-            line = cast(IO[str], process.stderr).readline().strip()
-
+        for line in cast(IO[str], process.stderr):
             if (retcode := process.poll()) is not None:
                 if retcode != 0:
                     raise ffmpeg.Error('ffmpeg', None, line)
                 break
 
-            if line:
-                logger.debug(line)
+            logger.debug(line)
 
-                if items := {key: value
-                            for key, value in progress_pattern.findall(line) if value != 'N/A'}:
-                    yield cast(ProgressItem, items)
+            if items := {key: value
+                        for key, value in progress_pattern.findall(line) if value != 'N/A'}:
+                yield cast(ProgressItem, items)

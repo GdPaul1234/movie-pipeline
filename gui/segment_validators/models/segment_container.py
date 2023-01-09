67c7ee93fc7b37e0f7f102db603adbe91aa35f23
GdPaul1234 <paul.godin1234@outlook.fr>
2023-01-09 22:43:09 +0100

    Validate segments when adding or editing segment

--- gui/segment_validators/models/segment_container.py
+++ gui/segment_validators/models/segment_container.py
@@ -19,6 +19,11 @@ class Segment:
             "{:02.0f}:{:02.0f}".format(*divmod(self.duration, 60))
         ))
 
+    def is_overlapping(self, other_segment: 'Segment'):
+        return (other_segment.start <= self.start <= other_segment.end \
+            or other_segment.start <= self.end <= other_segment.end) \
+            and other_segment != self
+
     @property
     def duration(self) -> float:
         return self.end - self.start
@@ -31,15 +36,21 @@ class SegmentContainer:
     def segments(self):
         return tuple(sorted(self._segments))
 
+    @staticmethod
+    def check_validity(segments, new_segment: Segment):
+        return not any(segment.is_overlapping(new_segment) for segment in segments)
+
     def add(self, segment: Segment):
-        self._segments.add(segment)
+        if self.check_validity(self._segments, segment):
+            self._segments.add(segment)
 
     def remove(self, segment: Segment):
         self._segments.remove(segment)
 
     def edit(self, old_segment: Segment, new_segment: Segment):
-        self._segments.remove(old_segment)
-        self._segments.add(new_segment)
+        if self.check_validity([segment for segment in self._segments if segment != old_segment], new_segment):
+            self._segments.remove(old_segment)
+            self._segments.add(new_segment)
 
     def merge(self, segments: list[Segment]):
         self._segments -= set(segments)

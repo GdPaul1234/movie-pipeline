1efe2585d2e7db482fb9ece229119d1fe294dd10
GdPaul1234 <paul.godin1234@outlook.fr>
2023-01-09 13:38:42 +0100

    Multi select segments

--- gui/segment_validators/views/segments_list.py
+++ gui/segment_validators/views/segments_list.py
@@ -9,7 +9,7 @@ from util import seconds_to_position
 right_click_menu = [
     '&Segments',
     [
-        '&Delete segment',
+        '&Delete segment(s)',
         '&Merge segments',
     ]
 ]
@@ -44,20 +44,9 @@ def render_values(window: sg.Window):
     window.write_event_value('-SEGMENTS-UPDATED-', True)
 
 
-def get_selected_segment(window: sg.Window) -> Segment|None:
-    segment_container = cast(SegmentContainer, window.metadata['segment_container'])
-    table = cast(sg.Table, window['-SEGMENTS-LIST-'])
-    row, _ = table.get_last_clicked_position()
-
-    if row is None or row == -1: return
-    if row >= len(segment_container.segments): return
-
-    return segment_container.segments[row]
-
-
-
 def handle_segments_list(window: sg.Window, event: str, values: dict[str, Any]):
     segment_container = cast(SegmentContainer, window.metadata['segment_container'])
+    table = cast(sg.Table, window['-SEGMENTS-LIST-'])
 
     if event == 'Add segment':
         player = window.metadata['media_player']
@@ -65,8 +54,10 @@ def handle_segments_list(window: sg.Window, event: str, values: dict[str, Any]):
         segment_container.add(Segment(current_position, current_position + 1))
         render_values(window)
 
-    elif event == 'Delete segment' and (selected_segment := get_selected_segment(window)):
-        segment_container.remove(selected_segment)
+    elif event == 'Delete segment(s)':
+        selected_segments = [segment_container.segments[row] for row in table.SelectedRows]
+        for selected_segment in selected_segments:
+            segment_container.remove(selected_segment)
         render_values(window)
 
     elif event == 'Merge segments':

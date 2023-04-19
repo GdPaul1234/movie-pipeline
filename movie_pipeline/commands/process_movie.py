093ce3e1fee27e8b1eb9040da4604222a928804b
GdPaul1234 <paul.godin1234@outlook.fr>
2023-04-19 23:29:13 +0200

    Fix valid filename

--- movie_pipeline/commands/process_movie.py
+++ movie_pipeline/commands/process_movie.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 
 edl_content_schema = Schema({
     # valid filename of the output file with .mp4 suffix
-    "filename": Regex(r"^[\w&àéèï'!(), -.]+\.mp4$"),
+    "filename": Regex(r"^[\w&àéèï'!(), #-.]+\.mp4$"),
     # format: hh:mm:ss.ss-hh:mm:ss.ss,hh-mm:ss…
     "segments": Regex(r'(?:(?:\d{2}:\d{2}:\d{2}\.\d{2,3})-(?:\d{2}:\d{2}:\d{2}\.\d{2,3}),)+'),
     Optional("skip_backup", default=False): bool

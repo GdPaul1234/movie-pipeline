ebecd56b7ad9650e816e28501729289d609800a6
GdPaul1234 <paul.godin1234@outlook.fr>
2023-05-12 19:26:30 +0200

    Relax nfo parser check + update dashboard

--- movie_pipeline/lib/nfo_parser.py
+++ movie_pipeline/lib/nfo_parser.py
@@ -41,7 +41,7 @@ class NfoGetter(ABC, GetterDict):
 class BaseNfo(BaseModel, ABC):
     title: str
     rating: NonNegativeInt
-    plot: str
+    plot: Optional[str]
     mpaa: Optional[str]
     genres: list[str]
     premiered: PastDate

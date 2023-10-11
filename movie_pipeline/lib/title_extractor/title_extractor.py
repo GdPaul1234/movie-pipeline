3188d8b4c8909f1914b1ccf18fe90142f16a287a
GdPaul1234 <paul.godin1234@outlook.fr>
2023-10-11 15:01:30 +0200

    Add title extractor interface

--- movie_pipeline/lib/title_extractor/title_extractor.py
+++ movie_pipeline/lib/title_extractor/title_extractor.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from pathlib import Path
 import json
 import re
@@ -16,44 +17,40 @@ def load_metadata(movie_path: Path):
         return json.loads(movie_metadata_path.read_text(encoding='utf-8'))
 
 
-class NaiveTitleExtractor:
+class ITitleExtractor(ABC):
     def __init__(self, title_cleaner: TitleCleaner) -> None:
         self._cleaner = title_cleaner
 
     def extract_title(self, movie_path: Path) -> str:
+        ...
+
+
+class NaiveTitleExtractor(ITitleExtractor):
+    def extract_title(self, movie_path: Path) -> str:
         return self._cleaner.clean_title(naive_title(movie_path, None, title_pattern=title_pattern))
 
 
-class SubtitleTitleExpanderExtractor:
+class SubtitleTitleExpanderExtractor(ITitleExtractor):
     title_pattern = re.compile(r"([^.]+)\.")
     episode_pattern = re.compile(r". '([^']+)'")
-
-    def __init__(self, title_cleaner: TitleCleaner) -> None:
-        self._cleaner = title_cleaner
 
     def extract_title(self, movie_path: Path) -> str:
         metadata = load_metadata(movie_path)
         return self._cleaner.clean_title(expanded_subtitle_title(movie_path, metadata, base_title_pattern=title_pattern, title_pattern=self.title_pattern, episode_pattern=self.episode_pattern))
 
 
-class SerieSubTitleAwareTitleExtractor:
+class SerieSubTitleAwareTitleExtractor(ITitleExtractor):
     episode_extractor_params = ('sub_title', re.compile(r'(\d+)[/-]\d+'))
     season_extractor_params = ('sub_title', re.compile(r'Saison (\d+)'))
-
-    def __init__(self, title_cleaner: TitleCleaner) -> None:
-        pass
 
     def extract_title(self, movie_path: Path) -> str:
         metadata = load_metadata(movie_path)
         return subtitle_aware_title(movie_path, metadata, title_pattern=title_pattern, episode_extractor_params=self.episode_extractor_params, season_extractor_params=self.season_extractor_params)
 
 
-class SerieTitleAwareTitleExtractor:
+class SerieTitleAwareTitleExtractor(ITitleExtractor):
     episode_extractor_params = ('title', re.compile(r'(\d+)-\d+'))
     season_extractor_params = ('title', re.compile(r'Saison (\d+)'))
-
-    def __init__(self, title_cleaner: TitleCleaner) -> None:
-        pass
 
     def extract_title(self, movie_path: Path) -> str:
         metadata = load_metadata(movie_path)

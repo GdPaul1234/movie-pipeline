e38be9529b690e0a5714e7f5e39169f48576e269
GdPaul1234 <paul.godin1234@outlook.fr>
2023-10-21 16:59:03 +0200

    Fix MovieSegments constructor for empty raw segments

--- movie_pipeline/models/movie_segments.py
+++ movie_pipeline/models/movie_segments.py
@@ -1,16 +1,24 @@
 from dataclasses import dataclass
 import itertools
+from typing import cast
 
 from util import position_in_seconds
 
 
+segment_t = tuple[float, float]
+
+
 @dataclass
 class MovieSegments:
-    segments: list[tuple[float, float]]
+    segments: list[segment_t]
 
     def __init__(self, raw_segments: str) -> None:
-        self.segments = [tuple(map(position_in_seconds, segment.split('-', 2)))
-                         for segment in raw_segments.removesuffix(',').split(',')] or []
+        splitted_raw_segments = raw_segments.removesuffix(',').split(',')
+
+        self.segments = [
+            cast(segment_t, tuple(position_in_seconds(x) for x in segment.split('-', 2)))
+            for segment in splitted_raw_segments
+        ] if splitted_raw_segments != [''] else []
 
     @property
     def total_seconds(self) -> float:

51591184b913471d9840960d8728226cab3150f4
GdPaul1234 <paul.godin1234@outlook.fr>
2024-09-07 20:37:55 +0200

    Set auto_detect strategy as default

--- movie_pipeline/main.py
+++ movie_pipeline/main.py
@@ -31,7 +31,7 @@ def archive_movies():
 @app.command('detect_segments')
 def detect_segments(
     filepath: Annotated[Path, typer.Argument(help='File or folder to process')],
-    detector: Annotated[list[DetectorKey],typer.Option(help='Run detect segments with selected detectors')] = [DetectorKey.match_template]
+    detector: Annotated[list[DetectorKey], typer.Option(help='Run detect segments with selected detectors')] = [DetectorKey.auto]
 ):
     """Run best-effort segments detectors"""
     from movie_pipeline.commands.detect_segments import command
